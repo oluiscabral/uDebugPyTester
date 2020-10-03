a0790c5d39c9fb0d6d4a948c7ccf478b29a57427
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2020-10-03 14:35:29 -0300

    Optimize tester process usage and creation

--- uDebugPyTester/PyTester.cs
+++ uDebugPyTester/PyTester.cs
@@ -19,10 +19,13 @@ namespace uDebugPyTester
 
         override public bool Test(int testAmount)
         {
+            TesterProcess inputProc = new TesterProcess(this.testModule, "python.exe", false);
+            TesterProcess outputProc = new TesterProcess(this.module, "python.exe", false);
+
             for (int i = 0; i < testAmount; i++)
             {
-                string input = generateOutput(this.testModule);
-                string output = generateOutput(this.module, input);
+                string input = inputProc.getOutput();
+                string output = outputProc.getOutput(input);
 
                 if (input is null || output is null)
                 {
@@ -38,53 +41,6 @@ namespace uDebugPyTester
                 }
             }
             return true;
-        }
-
-        private static string generateOutput(string module)
-        {
-            return generateOutput(module, null);
-        }
-
-        private static string generateOutput(string module, string input)
-        {
-            bool redirectStdInp = input != null;
-
-            StringBuilder output = new StringBuilder();
-
-            Process proc = new Process();
-            proc.StartInfo.FileName = "python.exe";
-            proc.StartInfo.RedirectStandardOutput = true;
-            proc.StartInfo.RedirectStandardInput = redirectStdInp;
-            proc.StartInfo.UseShellExecute = false;
-
-            proc.StartInfo.Arguments = string.Concat(module);
-            proc.Start();
-
-            if (redirectStdInp)
-            {
-                StreamWriter sWriter = proc.StandardInput;
-                sWriter.WriteLine(input);
-            }
-
-            StreamReader sReader = proc.StandardOutput;
-            string o = sReader.ReadToEnd();
-
-            proc.WaitForExit();
-
-            if (!string.IsNullOrEmpty(o))
-            {
-                output.Append(o).Replace("\r\n", "\n");
-                while (output[output.Length - 1].Equals('\n'))
-                {
-                    output.Remove(output.Length - 1, 1);
-                }
-            }
-            else
-            {
-                return null;
-            }
-
-            return output.ToString();
         }
     }
 }
---
+++ uDebugPyTester/TesterProcess.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace uDebugPyTester
+{
+    public class TesterProcess
+    {
+        private Process process = new Process();
+
+        public TesterProcess(string module, string fileName, bool useShellExecute)
+        {
+            process.StartInfo.FileName = fileName;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = useShellExecute;
+            process.StartInfo.Arguments = string.Concat(module);
+        }
+
+        public string getOutput()
+        {
+            return getOutput(null);
+        }
+
+        public string getOutput(string input)
+        {
+            this.process.StartInfo.RedirectStandardInput = input != null;
+
+            process.Start();
+
+            if (this.process.StartInfo.RedirectStandardInput)
+            {
+                StreamWriter sWriter = process.StandardInput;
+                sWriter.WriteLine(input);
+            }
+
+            StreamReader sReader = process.StandardOutput;
+            string output = sReader.ReadToEnd();
+
+            process.Close();
+
+            if (!string.IsNullOrEmpty(output))
+            {
+                return output.Replace("\r\n", "\n").TrimEnd();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string getStaticOutput(string module, string fileName)
+        {
+            return getStaticOutput(module, fileName, false);
+        }
+
+        public static string getStaticOutput(string module, string fileName, bool useShellExecute)
+        {
+            return getStaticOutput(module, fileName, useShellExecute, null);
+        }
+
+        public static string getStaticOutput(string module, string fileName, bool useShellExecute, string input)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = fileName;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = useShellExecute;
+            process.StartInfo.Arguments = string.Concat(module);
+            process.StartInfo.RedirectStandardInput = input != null;
+
+            process.Start();
+
+            if (process.StartInfo.RedirectStandardInput)
+            {
+                StreamWriter sWriter = process.StandardInput;
+                sWriter.WriteLine(input);
+            }
+
+            StreamReader sReader = process.StandardOutput;
+            string output = sReader.ReadToEnd();
+
+            process.Close();
+
+            if (!string.IsNullOrEmpty(output))
+            {
+                return output.Replace("\r\n", "\n").TrimEnd();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
